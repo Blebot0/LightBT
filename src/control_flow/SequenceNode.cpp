fcfdae722eabb73f39ff83bab6cedf982162a4ed
Blebot0 <keshav29kapur@gmail.com>
2023-03-23 23:31:01 +0530

    Added - Tick Traversal Updated - Sequence Node Source Description #1

--- src/control_flow/SequenceNode.cpp
+++ src/control_flow/SequenceNode.cpp
@@ -1,3 +1,17 @@
+/*
+* Author: Keshav Kapur
+* Date: 22 March 2023
+*
+* Short Description: Implementation of the sequence node in a behaviour tree
+* 
+* Detailed Description: A sequence node is a control flow node which handles the 
+* the flow of its child nodes. 
+* 
+* Success: All Children Nodes Succeed
+* Fails: One Child Fails
+* Running: If One Child return Running
+*/
+
 #include "control_flow/SequenceNode.h"
 
 using namespace BehaviourTree;
